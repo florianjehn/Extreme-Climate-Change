6cd14ef8dfc3c29126f64c34a945bb9a0d80f401
Florian Ulrich Jehn <florian.u.jehn@envr.jlug.de>
2020-12-09 14:31:31 +0100

    Implemented automating plotting for all ppm

--- calculate_probabilities.py
+++ calculate_probabilities.py
@@ -10,26 +10,72 @@ import numpy as np
 from sklearn.metrics import auc
 import os
 
-# Read in the extracted xy data from the weitzman curve
-curve_xy = pd.read_csv("Data" + os.sep + "wagner_weitzman_2015_700ppm.csv", sep=";")
 
-# Interpolate it to make it more smooth
-tck = interpolate.splrep(curve_xy["x"], curve_xy["y"],s=0)
-# make someovershoot to not get artifacts around 0 and 10
-x_new = np.arange(-2,12, 0.01)
-y_new = interpolate.splev(x_new, tck, der=0)
-curve_new = pd.DataFrame(data=[x_new, y_new]).transpose()
-curve_new.columns=["x", "y"]
-# cut it down to original size
-curve_new = curve_new.iloc[200:1301,:]
-curve_new.reset_index(inplace=True, drop=True)
-# Start is 0,0
-curve_new.iloc[0,0] = 0
-curve_new.iloc[0,1] = 0
-# Calculat the different probabilities.
-warming_prob = pd.DataFrame(index=[0])
+def read_data(ppm):
+    # Read in the extracted xy data from the weitzman curve
+    curve_xy = pd.read_csv("Data" + os.sep + "wagner_weitzman_2015_"+str(ppm)+"ppm.csv", sep=";")
+    return curve_xy
 
-# # by temperature
+
+def interpolate_curve(curve_xy):
+    # Interpolate it to make it more finely grained
+    tck = interpolate.splrep(curve_xy["x"], curve_xy["y"],s=0)
+    # make someovershoot to not get artifacts around 0 and 10
+    x_new = np.arange(-2,12, 0.01)
+    y_new = interpolate.splev(x_new, tck, der=0)
+    curve_new = pd.DataFrame(data=[x_new, y_new]).transpose()
+    curve_new.columns=["x", "y"]
+    # cut it down to original size
+    curve_new = curve_new.iloc[200:1301,:]
+    curve_new.reset_index(inplace=True, drop=True)
+    # Start is 0,0
+    curve_new.iloc[0,0] = 0
+    curve_new.iloc[0,1] = 0
+    # Return
+    return curve_new
+
+
+def get_probability(curve_new, ppm):
+    # Calculat the different probabilities.
+    warming_prob = pd.DataFrame(index=[0])
+    # by temperature .5°C
+    warming_prob["0.5°C"] = auc(curve_new.loc[25:75,"x"], curve_new.loc[25:75, "y"])
+    warming_prob["1°C"] = auc(curve_new.loc[75:125,"x"], curve_new.loc[75:125, "y"])
+    warming_prob["1.5°C"] = auc(curve_new.loc[125:175,"x"], curve_new.loc[125:175, "y"])
+    warming_prob["2°C"] = auc(curve_new.loc[175:225,"x"], curve_new.loc[175:225, "y"])
+    warming_prob["2.5°C"] = auc(curve_new.loc[225:275,"x"], curve_new.loc[225:275, "y"])
+    warming_prob["3°C"] = auc(curve_new.loc[275:325,"x"], curve_new.loc[275:325, "y"])
+    warming_prob["3.5°C"] = auc(curve_new.loc[325:375,"x"], curve_new.loc[325:375, "y"])
+    warming_prob["4°C"] = auc(curve_new.loc[375:425,"x"], curve_new.loc[375:425, "y"])
+    warming_prob["4.5°C"] = auc(curve_new.loc[425:475,"x"], curve_new.loc[425:475, "y"])
+    warming_prob["5°C"] = auc(curve_new.loc[475:525,"x"], curve_new.loc[475:525, "y"])
+    warming_prob["5.5°C"] = auc(curve_new.loc[525:575,"x"], curve_new.loc[525:575, "y"])
+    warming_prob["6°C"] = auc(curve_new.loc[575:625,"x"], curve_new.loc[575:625, "y"])
+    warming_prob["6.5°C"] = auc(curve_new.loc[625:675,"x"], curve_new.loc[625:675, "y"])
+    warming_prob["7°C"] = auc(curve_new.loc[675:725,"x"], curve_new.loc[675:725, "y"])
+    warming_prob["7.5°C"] = auc(curve_new.loc[725:775,"x"], curve_new.loc[725:775, "y"])
+    warming_prob["8°C"] = auc(curve_new.loc[775:825,"x"], curve_new.loc[775:825, "y"])
+    warming_prob["8.5°C"] = auc(curve_new.loc[825:875,"x"], curve_new.loc[825:875, "y"])
+    warming_prob["9°C"] = auc(curve_new.loc[875:925,"x"], curve_new.loc[875:925, "y"])
+    warming_prob["9.5°C"] = auc(curve_new.loc[925:975,"x"], curve_new.loc[925:975, "y"])
+    warming_prob["10°C"] = auc(curve_new.loc[975:1025,"x"], curve_new.loc[975:1025, "y"])
+    # Save    
+    warming_prob.transpose().to_csv("Results" +os.sep+ "warming_probabilities_"+str(ppm)+"ppm.csv",sep=";")
+
+
+if __name__ == "__main__":
+    # Run for all ppm
+    for ppm in np.arange(400, 1001, 50):
+        curve_xy = read_data(ppm)
+        curve_new = interpolate_curve(curve_xy)
+        get_probability(curve_new, ppm)
+        
+        
+        
+        
+        
+        
+# # by temperature group
 # warming_prob["< 1°C"] = auc(curve_new.loc[0:100,"x"], curve_new.loc[0:100, "y"])
 # warming_prob[">= 1°C < 2°C"] = auc(curve_new.loc[100:200,"x"], curve_new.loc[100:200, "y"])
 # warming_prob[">= 2°C < 3°C"] = auc(curve_new.loc[200:300,"x"], curve_new.loc[200:300, "y"])
@@ -42,27 +88,3 @@ warming_prob = pd.DataFrame(index=[0])
 # warming_prob[">= 9°C < 10°C"] = auc(curve_new.loc[900:1000,"x"], curve_new.loc[900:1000, "y"])
 # # Probability over 10°C is the 1 minus the rest
 # warming_prob[">= 10°C"] = 1-warming_prob.sum(axis=1)
-
-# by temperature .5°C
-warming_prob["0.5°C"] = auc(curve_new.loc[25:75,"x"], curve_new.loc[25:75, "y"])
-warming_prob["1°C"] = auc(curve_new.loc[75:125,"x"], curve_new.loc[75:125, "y"])
-warming_prob["1.5°C"] = auc(curve_new.loc[125:175,"x"], curve_new.loc[125:175, "y"])
-warming_prob["2°C"] = auc(curve_new.loc[175:225,"x"], curve_new.loc[175:225, "y"])
-warming_prob["2.5°C"] = auc(curve_new.loc[225:275,"x"], curve_new.loc[225:275, "y"])
-warming_prob["3°C"] = auc(curve_new.loc[275:325,"x"], curve_new.loc[275:325, "y"])
-warming_prob["3.5°C"] = auc(curve_new.loc[325:375,"x"], curve_new.loc[325:375, "y"])
-warming_prob["4°C"] = auc(curve_new.loc[375:425,"x"], curve_new.loc[375:425, "y"])
-warming_prob["4.5°C"] = auc(curve_new.loc[425:475,"x"], curve_new.loc[425:475, "y"])
-warming_prob["5°C"] = auc(curve_new.loc[475:525,"x"], curve_new.loc[475:525, "y"])
-warming_prob["5.5°C"] = auc(curve_new.loc[525:575,"x"], curve_new.loc[525:575, "y"])
-warming_prob["6°C"] = auc(curve_new.loc[575:625,"x"], curve_new.loc[575:625, "y"])
-warming_prob["6.5°C"] = auc(curve_new.loc[625:675,"x"], curve_new.loc[625:675, "y"])
-warming_prob["7°C"] = auc(curve_new.loc[675:725,"x"], curve_new.loc[675:725, "y"])
-warming_prob["7.5°C"] = auc(curve_new.loc[725:775,"x"], curve_new.loc[725:775, "y"])
-warming_prob["8°C"] = auc(curve_new.loc[775:825,"x"], curve_new.loc[775:825, "y"])
-warming_prob["8.5°C"] = auc(curve_new.loc[825:875,"x"], curve_new.loc[825:875, "y"])
-warming_prob["9°C"] = auc(curve_new.loc[875:925,"x"], curve_new.loc[875:925, "y"])
-warming_prob["9.5°C"] = auc(curve_new.loc[925:975,"x"], curve_new.loc[925:975, "y"])
-warming_prob["10°C"] = auc(curve_new.loc[975:1025,"x"], curve_new.loc[975:1025, "y"])
-
-warming_prob.transpose().to_csv("Results" +os.sep+ "warming_probabilities.csv",sep=";")
--- comparison_plot.py
+++ comparison_plot.py
@@ -8,6 +8,8 @@ Created on Sat Dec  5 13:32:12 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+
 
 def plot_nicer(ax, with_legend=True):
   """Takes an axis objects and makes it look nicer"""
@@ -31,95 +33,113 @@ def plot_nicer(ax, with_legend=True):
   ax.yaxis.grid(True, color="lightgrey", zorder=0)
   ax.xaxis.grid(False)
 
-# Read in the data
-prob_temp = pd.read_csv("Results" + os.sep + "warming_probabilities.csv", sep=";", index_col=0)
-ipcc_counts = pd.read_csv("Results" + os.sep + "temp_counts_all.csv", sep=";", index_col=0)
-counts_1_5_report = pd.read_csv("Results" + os.sep + "counts_SR15_Full_Report_High_Res.csv", sep=";",index_col=0)
 
-# Create a gridspec to plot in
-fig = plt.figure()
-gs = fig.add_gridspec(2,4)
-ax1 = fig.add_subplot(gs[0,:])
-ax2 = fig.add_subplot(gs[1,:2])
-ax3 = fig.add_subplot(gs[1,2])
-ax4 = fig.add_subplot(gs[1,3])
-
-# Replace the spaces in the temperature description
-ipcc_counts.index = ipcc_counts.index.str.replace(" ","")
-counts_1_5_report.index = counts_1_5_report.index.str.replace(" ","")
-
-# Convert probability to percent
-prob_temp = prob_temp * 100
-
-##### Preparation for the count without 1_5 report
-without_1_5 = pd.DataFrame(ipcc_counts[ipcc_counts.columns[0]] - 
-                           counts_1_5_report[counts_1_5_report.columns[0]])
-without_1_5_total = without_1_5.sum()
-# Convert to percent
-without_1_5[without_1_5.columns[0]] = (without_1_5/without_1_5_total) * 100
-# merge
-compare_df_without_1_5 = without_1_5.merge(prob_temp,left_index=True, right_index=True)
-compare_df_without_1_5.columns = ["Relative occurence in IPCC reports", "Probability of warming"]
+def read_counts():
+    # Read in the data
+    ipcc_counts = pd.read_csv("Results" + os.sep + "temp_counts_all.csv", sep=";", index_col=0)
+    counts_1_5_report = pd.read_csv("Results" + os.sep + "counts_SR15_Full_Report_High_Res.csv", sep=";",index_col=0)
+    
+    # Replace the spaces in the temperature description
+    ipcc_counts.index = ipcc_counts.index.str.replace(" ","")
+    counts_1_5_report.index = counts_1_5_report.index.str.replace(" ","")
+    return ipcc_counts, counts_1_5_report
 
 
-##### Preparation for the total counts
-ipcc_total = ipcc_counts.sum()
-# Convert counts to percent
-ipcc_counts[ipcc_counts.columns[0]] = (ipcc_counts / ipcc_total) * 100
-# merge
-compare_df = ipcc_counts.merge(prob_temp,left_index=True, right_index=True)
-compare_df.columns = ["Relative occurence in IPCC reports", "Probability of warming"]
+def read_probability(ppm):
+    prob_temp = pd.read_csv("Results" + os.sep + "warming_probabilities_"+ str(ppm)+"ppm.csv", sep=";", index_col=0)
+    return prob_temp
 
 
+def prepare_data(prob_temp, ipcc_counts, counts_1_5_report):
+    # Convert probability to percent
+    prob_temp = prob_temp * 100
+    
+    ##### Preparation for the count without 1_5 report
+    without_1_5 = pd.DataFrame(ipcc_counts[ipcc_counts.columns[0]] - 
+                               counts_1_5_report[counts_1_5_report.columns[0]])
+    without_1_5_total = without_1_5.sum()
+    # Convert to percent
+    without_1_5[without_1_5.columns[0]] = (without_1_5/without_1_5_total) * 100
+    # merge
+    compare_df_without_1_5 = without_1_5.merge(prob_temp,left_index=True, right_index=True)
+    compare_df_without_1_5.columns = ["Relative occurence in IPCC reports", "Probability of warming"]
+    
+    
+    ##### Preparation for the total counts
+    ipcc_total = ipcc_counts.sum()
+    # Convert counts to percent
+    ipcc_counts[ipcc_counts.columns[0]] = (ipcc_counts / ipcc_total) * 100
+    # merge
+    compare_df = ipcc_counts.merge(prob_temp,left_index=True, right_index=True)
+    compare_df.columns = ["Relative occurence in IPCC reports", "Probability of warming"]
+    
+    ##### Preparation for the >=6°C and >=3°C Plot
+    over_6 = pd.DataFrame(compare_df.iloc[11:].sum()).transpose()
+    over_3 = pd.DataFrame(compare_df.iloc[5:].sum()).transpose()
+    
+    return compare_df, compare_df_without_1_5, over_3, over_6
 
 
-##### Preparation for the >=6°C and >=3°C Plot
-# Only use the values for 6 and above
-over_6 = pd.DataFrame(compare_df.iloc[11:].sum()).transpose()
-# The sum of probability is slightly above 10 because of the way it is 
-# calculated from the pdf. Set to 10 here, so it is in line with Wagner and 
-# Weitzmann
-over_6.iloc[0,1] = 10
-over_3 = pd.DataFrame(compare_df.iloc[5:].sum()).transpose()
-
-
-##### Plot the total count
-compare_df.plot(kind="bar", ax=ax1,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black" )
-ax1.set_title("a) Temperature count in AR5 working group reports and special reports until 2020")
-
-
-#### Plot without the 1.5 special report
-compare_df_without_1_5.plot(kind="bar", ax=ax2, legend=False,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black")
-ax2.set_title("b) Excluding special report on 1.5°C warming")
-plt.setp(ax2.xaxis.get_majorticklabels(), fontsize=6)
-
-
-#### Plot 3
-over_3.plot(kind="bar", ax=ax3, width=0.1, legend=False,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black")
-ax3.set_title("c) 3°C and above")
-plt.setp(ax3.xaxis.get_majorticklabels(), color="white")
-
-
-#### Plot only 6 degrees and above
-over_6.plot(kind="bar", ax=ax4, width=0.1, legend=False,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black")
-ax4.set_title("d) 6°C and above")
-plt.setp(ax4.xaxis.get_majorticklabels(), color="white")
-
-
-
-# make nicer
-i = 0
-for ax in [ax1, ax2, ax3, ax4]:
-    ax.set_ylabel("Percentage [%]")
-    if i == 0:
-        plot_nicer(ax)
-    else: 
-        plot_nicer(ax, with_legend=False)
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)
-    i +=1
-
-fig=plt.gcf()
-fig.set_size_inches(12,6)
-fig.tight_layout()
-plt.savefig("warming_count.png",dpi=200, bbox_inches="tight")
+def plot_figures(compare_df, compare_df_without_1_5, over_3, over_6, ppm):  
+    """Plots the main figures for the different ppm"""  
+    # Create a gridspec to plot in
+    fig = plt.figure()
+    gs = fig.add_gridspec(2,4)
+    ax1 = fig.add_subplot(gs[0,:])
+    ax2 = fig.add_subplot(gs[1,:2])
+    ax3 = fig.add_subplot(gs[1,2])
+    ax4 = fig.add_subplot(gs[1,3])
+    
+    ##### Plot the total count
+    compare_df.plot(kind="bar", ax=ax1,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black" )
+    ax1.set_title("a) Temperature count in AR5 working group reports and special reports until 2020")
+        
+    #### Plot without the 1.5 special report
+    compare_df_without_1_5.plot(kind="bar", ax=ax2, legend=False,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black")
+    ax2.set_title("b) Excluding special report on 1.5°C warming")
+    plt.setp(ax2.xaxis.get_majorticklabels(), fontsize=6)
+       
+    #### Plot 3
+    over_3.plot(kind="bar", ax=ax3, width=0.1, legend=False,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black")
+    ax3.set_title("c) 3°C and above")
+    plt.setp(ax3.xaxis.get_majorticklabels(), color="white")
+     
+    #### Plot only 6 degrees and above
+    over_6.plot(kind="bar", ax=ax4, width=0.1, legend=False,zorder=5, color=["#7395F4", "#DE624E"], edgecolor="black")
+    ax4.set_title("d) 6°C and above")
+    plt.setp(ax4.xaxis.get_majorticklabels(), color="white")
+    
+    # make nicer
+    i = 0
+    for ax in [ax1, ax2, ax3, ax4]:
+        ax.set_ylabel("Percentage [%]")
+        if i == 0:
+            plot_nicer(ax)
+        else: 
+            plot_nicer(ax, with_legend=False)
+        plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)
+        i +=1
+    
+    fig=plt.gcf()
+    fig.set_size_inches(12,6)
+    fig.tight_layout()
+    plt.savefig("Figures" + os.sep + "warming_count_"+str(ppm)+".png",dpi=200, bbox_inches="tight")
+    plt.close()
+    
+if __name__ == "__main__":
+    # Read the data
+    ipcc_counts, counts_1_5_report = read_counts()
+    
+    # Run for all ppm
+    for ppm in np.arange(400, 1001, 50):
+        prob_temp = read_probability(ppm)
+        compare_df, compare_df_without_1_5, over_3, over_6 = prepare_data(
+            prob_temp, ipcc_counts, counts_1_5_report)
+        plot_figures(compare_df, compare_df_without_1_5, over_3, over_6, ppm)
+        
+    
+    
+    
+    
+    
 
