d5df1d64680600d8d86f2dcfefde39e3c77698be
Florian Ulrich Jehn <florian.u.jehn@envr.jlug.de>
2021-01-25 13:43:30 +0100

    Finished new version of figure 1

--- figure_1.py
+++ figure_1.py
@@ -9,137 +9,211 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import calculate_probabilities as cp
+from sklearn.metrics import auc
 
 
 def plot_nicer(ax, with_legend=True):
-  """Takes an axis objects and makes it look nicer"""
-  alpha=0.7
-  for spine in ax.spines.values():
-    spine.set_color("lightgray")
-  # Make text grey
-  plt.setp(ax.get_yticklabels(), alpha=alpha)
-  plt.setp(ax.get_xticklabels(), alpha=alpha)
-  ax.set_xlabel(ax.get_xlabel(), alpha=alpha)
-  ax.set_ylabel(ax.get_ylabel(), alpha=alpha)
-  ax.set_title(ax.get_title(), alpha=alpha)
-  ax.tick_params(axis=u'both', which=u'both',length=0)
-  if with_legend:
-    legend = ax.get_legend()
-    for text in legend.get_texts():
-      text.set_color("#676767")
-    legend.get_title().set_color("#676767")
-  ax.yaxis.get_offset_text().set_color("#676767")
-  # Add a grid
-  ax.yaxis.grid(True, color="lightgrey", zorder=0)
-  ax.xaxis.grid(False)
+    """Takes an axis objects and makes it look nicer"""
+    alpha=0.7
+    for spine in ax.spines.values():
+      spine.set_color("lightgray")
+    # Make text grey
+    plt.setp(ax.get_yticklabels(), alpha=alpha)
+    plt.setp(ax.get_xticklabels(), alpha=alpha)
+    ax.set_xlabel(ax.get_xlabel(), alpha=alpha)
+    ax.set_ylabel(ax.get_ylabel(), alpha=alpha)
+    ax.set_title(ax.get_title(), alpha=alpha)
+    ax.tick_params(axis=u'both', which=u'both',color="#676767")
+    if with_legend:
+      legend = ax.get_legend()
+      for text in legend.get_texts():
+        text.set_color("#676767")
+      legend.get_title().set_color("#676767")
+    ax.yaxis.get_offset_text().set_color("#676767")
 
 
-def read_counts():
+
+def read_counts_total():
     # Read in the data
     ipcc_counts = pd.read_csv("Results" + os.sep + "temp_counts_all.csv", sep=";", index_col=0)
-    counts_1_5_report = pd.read_csv("Results" + os.sep + "counts_SR15_Full_Report_High_Res.csv", sep=";",index_col=0)
     
     # Replace the spaces in the temperature description
     ipcc_counts.index = ipcc_counts.index.str.replace(" ","")
+    # Make temperatures numerical
+    ipcc_counts.index = ipcc_counts.index.str.replace("°C","")
+    ipcc_counts.reset_index(inplace=True)
+    ipcc_counts.columns=["Temp Rise", "Count"]
+    ipcc_counts["Temp Rise"] = ipcc_counts["Temp Rise"].astype(float)
+    return ipcc_counts
+
+
+def read_counts_1_5():
+    # Read in the data
+    counts_1_5_report = pd.read_csv("Results" + os.sep + "counts_SR15_Full_Report_High_Res.csv", sep=";", index_col=0)
+    # Replace the spaces in the temperature description
     counts_1_5_report.index = counts_1_5_report.index.str.replace(" ","")
-    return ipcc_counts, counts_1_5_report
+    # Make temperatures numerical
+    counts_1_5_report.index = counts_1_5_report.index.str.replace("°C","")
+    counts_1_5_report.reset_index(inplace=True)
+    counts_1_5_report.columns=["Temp Rise", "Count"]
+    counts_1_5_report["Temp Rise"] = counts_1_5_report["Temp Rise"].astype(float)
+    return counts_1_5_report
 
 
-def read_probability(ppm):
-    prob_temp = pd.read_csv("Results" + os.sep + "warming_probabilities_"+ str(ppm)+"ppm.csv", sep=";", index_col=0)
+def read_probability_curves(ppm):
+    prob_temp = pd.read_csv("Data" + os.sep + "wagner_weitzman_2015_"+ str(ppm)+"ppm.csv", sep=";", index_col=0)
     return prob_temp
 
 
-def prepare_data(prob_temp, ipcc_counts, counts_1_5_report):
-    # Convert probability to percent
-    prob_temp = prob_temp * 100
-    
-    ##### Preparation for the count without 1_5 report
-    without_1_5 = pd.DataFrame(ipcc_counts[ipcc_counts.columns[0]] - 
-                               counts_1_5_report[counts_1_5_report.columns[0]])
-    without_1_5_total = without_1_5.sum()
-    # Convert to percent
-    without_1_5[without_1_5.columns[0]] = (without_1_5/without_1_5_total) * 100
-    # merge
-    compare_df_without_1_5 = without_1_5.merge(prob_temp,left_index=True, right_index=True)
-    compare_df_without_1_5.columns = ["Relative occurence in IPCC reports", "Probability of warming"]
-    
-    
-    ##### Preparation for the total counts
-    ipcc_total = ipcc_counts.sum()
+def prepare_data_a():  
+    total_counts = read_counts_total()
+    curve_550 = cp.interpolate_curve(cp.read_data(550))
+    curve_700 = cp.interpolate_curve(cp.read_data(700))
+    return total_counts, curve_550, curve_700
+
+
+def prepare_data_b():
+    total_counts = read_counts_total()
+    counts_1_5 = read_counts_1_5()
+    without_1_5 = pd.DataFrame(total_counts[total_counts.columns[-1]] - counts_1_5[counts_1_5.columns[-1]])
+    without_1_5["Temp Rise"] = total_counts["Temp Rise"]  
+    without_1_5.columns = ["Count", "Temp Rise"]
+    curve_550 = cp.interpolate_curve(cp.read_data(550))
+    curve_700 = cp.interpolate_curve(cp.read_data(700))    
+    return without_1_5, curve_550, curve_700
+
+
+def prepare_data_c():
+    total_counts = read_counts_total()
+    total = total_counts.sum()
     # Convert counts to percent
-    ipcc_counts_percent = (ipcc_counts / ipcc_total) * 100
-    # merge
-    compare_df = ipcc_counts_percent.merge(prob_temp,left_index=True, right_index=True)
-    compare_df.columns = ["Relative occurence in IPCC reports", "Probability of warming"]
+    total_counts_percent = (total_counts / total) * 100
+    count_over_3 = pd.DataFrame(total_counts_percent.iloc[5:].sum()).transpose()
     
-    ##### Preparation for the >=6°C and >=3°C Plot
-    over_6 = pd.DataFrame(compare_df.iloc[11:].sum()).transpose()
-    over_3 = pd.DataFrame(compare_df.iloc[5:].sum()).transpose()
+    # Get the probability of the values above 3°C
+    curve_700 = cp.interpolate_curve(cp.read_data(700))    
+    prob_over_3 = auc(curve_700.loc[300:,"x"], curve_700.loc[300:, "y"])
     
-    return compare_df, compare_df_without_1_5, over_3, over_6
+    return count_over_3.loc[0,"Count"], prob_over_3
+    
 
 
-def plot_figures(compare_df, compare_df_without_1_5, over_3, over_6, ppm, 
-                 color_prob, color_count, edgecolor):  
+def prepare_data_d():
+    total_counts = read_counts_total()
+    total = total_counts.sum()
+    # Convert counts to percent
+    total_counts_percent = (total_counts / total) * 100
+    count_over_6 = pd.DataFrame(total_counts_percent.iloc[11:].sum()).transpose()
+    
+    # Get the probability of the values above 3°C
+    curve_700 = cp.interpolate_curve(cp.read_data(700))    
+    prob_over_6 = auc(curve_700.loc[600:,"x"], curve_700.loc[600:, "y"])
+    
+    return count_over_6.loc[0,"Count"], prob_over_6
+
+def create_grid():  
     """Plots the main figures for the different ppm"""  
     # Create a gridspec to plot in
     fig = plt.figure()
-    gs = fig.add_gridspec(2,4)
+    gs = fig.add_gridspec(2,4,wspace=1.6,hspace=0.5)
     ax1 = fig.add_subplot(gs[0,:])
     ax2 = fig.add_subplot(gs[1,:2])
     ax3 = fig.add_subplot(gs[1,2])
     ax4 = fig.add_subplot(gs[1,3])
+    axes = [ax1, ax2, ax3, ax4]
+    return axes
+
+
+def plot_figure(color_prob, color_count, edgecolor):
+    axes = create_grid()
     
-    ##### Plot the total count
-    compare_df.plot(kind="bar", ax=ax1,zorder=5, color=[color_prob, color_count], edgecolor=edgecolor )
-    ax1.set_title("a) Temperature count in AR5 working group reports and special reports until 2020")
-        
-    #### Plot without the 1.5 special report
-    compare_df_without_1_5.plot(kind="bar", ax=ax2, legend=False,zorder=5, color=[color_prob, color_count], edgecolor=edgecolor)
-    ax2.set_title("b) Excluding special report on 1.5°C warming")
-    plt.setp(ax2.xaxis.get_majorticklabels(), fontsize=6)
-       
-    #### Plot 3
-    over_3.plot(kind="bar", ax=ax3, width=0.1, legend=False,zorder=5, color=[color_prob, color_count], edgecolor=edgecolor)
-    ax3.set_title("c) 3°C and above")
-    plt.setp(ax3.xaxis.get_majorticklabels(), color="white")
-     
-    #### Plot only 6 degrees and above
-    over_6.plot(kind="bar", ax=ax4, width=0.1, legend=False,zorder=5, color=[color_prob, color_count], edgecolor=edgecolor)
-    ax4.set_title("d) 6°C and above")
-    plt.setp(ax4.xaxis.get_majorticklabels(), color="white")
+    # Get data for fig a)
+    total_counts, curve_550, curve_700 = prepare_data_a()
+    # Plot fig a)
+    axes[0].bar(x=total_counts["Temp Rise"],height=total_counts["Count"], width=0.3,color=color_count,label="Occurence in IPCC reports")
+    axes.append(axes[0].twinx())
+    axes[4].plot(curve_550["x"], curve_550["y"], color=color_prob, linestyle="--", label="Probability curve of warming for 550 ppm")
+    axes[4].plot(curve_700["x"], curve_700["y"], color=color_prob, label="Probability curve of warming for 700 ppm")
+    axes[4].set_xlim(-0.5,10.5)
+    axes[4].set_ylim(0,0.4)
+    axes[4].bar(2,0,color=color_count,label="Occurence in IPCC reports")
+    legend = plt.legend()
+    # make legend nicer
+    for text in legend.get_texts():
+        text.set_color("#676767")
+    
+    # Get data for fig b)
+    without_1_5, curve_550, curve_700 = prepare_data_b()
+    axes[1].bar(x=without_1_5["Temp Rise"],height=without_1_5["Count"], width=0.3,color=color_count)
+    axes.append(axes[1].twinx())
+    axes[5].plot(curve_550["x"], curve_550["y"], color=color_prob, linestyle="--", label="550 ppm")
+    axes[5].plot(curve_700["x"], curve_700["y"], color=color_prob, label="700 ppm")
+    axes[5].set_xlim(-0.5,10.5)
+    axes[5].set_ylim(0,0.4)
+    
+    # Get data for fig c)
+    count_over_3, prob_over_3 = prepare_data_c()
+    axes[2].bar(x=1.5,height=count_over_3,width=0.04, color=color_count)
+    axes.append(axes[2].twinx())
+    axes[6].bar(x=1.6,height=prob_over_3,width=0.04, color=color_prob)
+    axes[2].set_ylim(0,65)
+    axes[6].set_ylim(0,0.65)
+    axes[2].set_xlim(1.35, 1.75)
+    axes[2].set_xticklabels([""])
+
+    
+    # Get data for fig d)
+    count_over_6, prob_over_6 = prepare_data_d()
+    axes[3].bar(x=1.5,height=count_over_6,width=0.04, color=color_count)
+    axes.append(axes[3].twinx())
+    axes[7].bar(x=1.6,height=prob_over_6,width=0.04, color=color_prob)
+    axes[3].set_ylim(0,65)
+    axes[7].set_ylim(0,0.65)
+    axes[3].set_xlim(1.35, 1.75)
+    axes[3].set_xticklabels([""])
+    
+    # Set lables
+    for ax in [axes[0], axes[1]]:
+        ax.set_ylabel("Occurence [/]")
+        ax.set_xlabel("Temperature Change [°C]")
+        ax.locator_params(axis='x', nbins=20)
+    
+    for ax in [axes[2],axes[3]]:
+        ax.set_ylabel("Relative Occurence [%]")
+
+    for ax in axes[4:]:
+        ax.set_ylabel("Probability [/]")
+    
+    axes[0].set_title("a) Temperature count in AR5 working group reports and special reports until 2020")
+    axes[1].set_title("b) Excluding special report on 1.5°C warming")
+    axes[2].set_title("c) 3°C and above")
+    axes[3].set_title("d) 6°C and above")
+    
+    
     
     # make nicer
     i = 0
-    for ax in [ax1, ax2, ax3, ax4]:
-        ax.set_ylabel("Percentage [%]")
-        if i == 0:
-            plot_nicer(ax)
-        else: 
-            plot_nicer(ax, with_legend=False)
+    for ax in axes:
+        plot_nicer(ax, with_legend=False)
         plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)
-        i +=1
     
+    for ax in [axes[2], axes[3], axes[6], axes[7]]:
+        ax.tick_params(axis="x", which=u'both',length=0)
+
     fig=plt.gcf()
     fig.set_size_inches(12,6)
     fig.tight_layout()
-    plt.savefig("Figures" + os.sep + "warming_count_"+str(ppm)+".png",dpi=200, bbox_inches="tight")
+    plt.savefig("Figures" + os.sep + "warming_curves.png",dpi=200, bbox_inches="tight")
     plt.close()
     
 if __name__ == "__main__":
     # Read the data
-    ipcc_counts, counts_1_5_report = read_counts()
-    color_prob = "#BD7F37FF"
-    color_count = "#A13941FF"
+    color_prob = "#3A3A3A"
+    color_count = "#BD7F37FF"
     edgecolor = "white"
-    # Run for all ppm
-    for ppm in np.arange(400, 1001, 50):
-        prob_temp = read_probability(ppm)
-        compare_df, compare_df_without_1_5, over_3, over_6 = prepare_data(
-            prob_temp, ipcc_counts, counts_1_5_report)
-        plot_figures(compare_df, compare_df_without_1_5, over_3, over_6, ppm, 
-                     color_prob, color_count, edgecolor)
+
+    plot_figure(color_prob, color_count, edgecolor)
         
     
     
